7c5d79e12aa452f62026cc6cbd33f5ebbc252634
Patryk <44586255+CreatorLuXury@users.noreply.github.com>
2019-03-19 19:19:02 +0100

    Update strcon.c

--- strcon.c
+++ strcon.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
+int strlen2(const char *str)
+{
+    int count = 0;
+    while (*(++str) != '\0') ++count;
+    return count;
+}
 
 void constr(char *s1, char *s2)
 {
-	int str1 = strlen(s1)-1;
-	int str2 = strlen(s2);
+	int str1 = strlen2(s1);
+	int str2 = strlen2(s2)+1;
 
 	int i;
 	for (i = 0; i < str2; i++)
